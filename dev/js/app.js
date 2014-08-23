3d9290eb24e1aba6edd74083711d63b71cdb000e
Sven Anders Robbestad <robbestad@gmail.com>
2014-08-23 12:36:28 +0200

    modal trigger

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,32 +1,40 @@
-(function(){
+(function () {
     'use strict';
 })();
 
 //initiating jQuery
-jQuery(function($) {
+jQuery(function ($) {
 
-(setNavbar=function(){
-  $(".navbar ul>li").css("width",($(".content").width()/3)-2+"px");
-})();
 
-$(document).ready( function() {
-  //enabling stickUp on the '.navbar-wrapper' class
-  $('.navbar-wrapper').stickUp();
-  //setting the navbar width
-  setNavbar();
-});
-$( window ).resize(function() {
-   setNavbar();
-});
+    (setNavbar = function () {
+        $(".navbar ul>li").css("width", ($(".content").width() / 3) - 2 + "px");
+    })();
 
+    $(document).ready(function () {
+        //enabling stickUp on the '.navbar-wrapper' class
+        $('.navbar-wrapper').stickUp();
+        //setting the navbar width
+        setNavbar();
+    });
+    $(window).resize(function () {
+        setNavbar();
+    });
+
+    $(".modal-event").on('click',function(){
+        console.log($(this).attr("src"));
+        $(".modal-img").attr("src", $(this).attr("src"));
+
+    });
 
     $(window).bind('load', function () {
-            parallaxInit();
+        parallaxInit();
     });
     var headerParallax = jQuery('.masthead');
+
     function parallaxInit() {
         headerParallax.parallax("50%", 1);
     }
+
     parallaxInit();
 
 
